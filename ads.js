887a2fc784b5ff09b5ffa3bac8b07f531722d9af
aryapen <aryaputrarizky4@gmail.com>
2026-06-26 13:29:29 +0700

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,1 +1,7 @@
-window.isAdsAvailable = true;
+// ads.js - Jebakan Anti-AdBlock
+// File ini sengaja dinamai 'ads.js' agar diblokir otomatis oleh ekstensi AdBlocker.
+
+(function() {
+    // Daftarkan variabel global secara aman
+    window.isAdsAvailable = true;
+})();
